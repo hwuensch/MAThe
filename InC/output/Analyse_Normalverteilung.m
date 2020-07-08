03d72527ba4a06499f93838bc033816f5b8bf49a
hendrik.wuensch@web.de <wuensch.h@smail.uni-koeln.de>
2020-07-08 16:00:11 +0200

    Matlab: erste graphische Analysefunktion

---
+++ InC/output/Analyse_Normalverteilung.m
@@ -0,0 +1,25 @@
+iterAll    = 500;
+dimension  = 2;
+startvalue = 3;
+world_rank = 0;
+%%
+filename = sprintf('iter%d_dim%d_start%d_rank%d.txt',iterAll,dimension,startvalue,world_rank)
+fileLog = importfileLog(filename, dimension);
+
+%%
+n = 100;
+x = linspace(min(fileLog(:,1)),max(fileLog(:,1)),n);
+y = linspace(min(fileLog(:,2)),max(fileLog(:,2)),n);
+[X,Y] = meshgrid(x,y);
+xx = reshape(X,[],1);
+yy = reshape(Y,[],1);
+Z = reshape(mvnpdf([xx yy]),n,n);
+
+figure
+hold on
+scatter(fileLog(:,1),fileLog(:,2))
+contour3(X,Y,Z)
+
+grid on
+
+%%
